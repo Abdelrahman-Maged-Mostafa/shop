fa3b6fb03b025b636f73f3a4c8d4901a73007bd4
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2024-09-30 12:50:19 +0300

    worked

--- src/hooks/Icone.jsx
+++ src/hooks/Icone.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import { useOptions } from "../context/useOptions";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const Icon = () => {
-  const { icon } = useOptions();
+  const { icon, initialSEOData } = useOptions();
   useEffect(() => {
     const link = document.createElement("link");
     link.rel = "icon";
@@ -16,7 +17,18 @@ const Icon = () => {
     };
   }, [icon]);
 
-  return null;
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>{initialSEOData?.homeTitle || "shop"}</title>
+        <meta
+          name="description"
+          content={initialSEOData?.homeDescription || ""}
+        />
+        <meta name="keywords" content={initialSEOData?.homeKeywords || ""} />
+      </Helmet>
+    </HelmetProvider>
+  );
 };
 
 export default Icon;
