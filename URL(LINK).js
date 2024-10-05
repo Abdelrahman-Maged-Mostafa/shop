351f85dc989fcabda3e774437f481cda2cc93d72
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2024-10-05 19:49:19 +0300

    worked

--- URL(LINK).js
+++ URL(LINK).js
@@ -1,4 +1,4 @@
 export const URL = "https://shop-backend-tau.vercel.app";
-export const FRONT_URL = "https://megamart-online.netlify.app";
+export const FRONT_URL = "https://koara-shop.netlify.app";
 
 // export const URL = "http://127.0.0.1:8000";
